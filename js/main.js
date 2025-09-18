41c71a02df077ca6f14ea3f5645c359b4481da81
mayanksir20 <mayanksir1995@gmail.com>
2025-09-18 18:09:20 +0530

    first commit

---
+++ js/main.js
@@ -0,0 +1,91 @@
+(function ($) {
+    "use strict";
+
+    // Spinner
+    var spinner = function () {
+        setTimeout(function () {
+            if ($('#spinner').length > 0) {
+                $('#spinner').removeClass('show');
+            }
+        }, 1);
+    };
+    spinner();
+
+
+    // Initiate the wowjs
+    new WOW().init();
+
+
+    // Sticky Navbar
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.sticky-top').addClass('shadow-sm').css('top', '0px');
+        } else {
+            $('.sticky-top').removeClass('shadow-sm').css('top', '-100px');
+        }
+    });
+
+
+    // Back to top button
+    $(window).scroll(function () {
+        if ($(this).scrollTop() > 300) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
+        }
+    });
+    $('.back-to-top').click(function () {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
+        return false;
+    });
+
+
+    // Facts counter
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 2000
+    });
+
+
+    // Testimonials carousel
+    $(".testimonial-carousel").owlCarousel({
+        autoplay: true,
+        smartSpeed: 1000,
+        items: 1,
+        dots: false,
+        loop: true,
+        nav: true,
+        navText: [
+            '<i class="bi bi-chevron-left"></i>',
+            '<i class="bi bi-chevron-right"></i>'
+        ]
+    });
+
+
+})(jQuery);
+
+
+// Counter Animation for multiple counters
+
+function counter(id, start, end, duration) {
+    let obj = document.getElementById(id);
+    let current = start;
+    let range = end - start;
+    let increment = end > start ? 1 : -1;
+    let step = Math.abs(Math.floor(duration / range));
+    let timer = setInterval(() => {
+        current += increment;
+        obj.textContent = current.toLocaleString();
+        if (current == end) {
+            clearInterval(timer);
+        }
+    }, step);
+}
+
+window.onload = () => {
+     counter("count", 19000, 20000, 2500); // Youth Trained
+    counter("count1", 19000, 20000, 2500); // Youth Trained
+    counter("count2", 24000, 25000, 2200); // RPL Certificates
+    counter("count3", 7000, 8000, 2000);  // Livelihoods Created
+    counter("count4", 0, 500, 1800);   // Communities Impacted
+};
